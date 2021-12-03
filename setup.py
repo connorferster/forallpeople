dc6c5f157fbd88f3ea283177100296f66c20b336
Connor Ferster <connorferster@gmail.com>
2021-12-03 15:24:27 -0800

    bump version for fix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/connorferster/forallpeople"
 EMAIL = "connorferster@gmail.com"
 AUTHOR = "Connor Ferster"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "2.4.1"
+VERSION = "2.4.2"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
