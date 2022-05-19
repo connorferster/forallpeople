6454242e395ef8ec6165080659032d18a47100fd
Connor Ferster <connorferster@gmail.com>
2022-05-18 22:32:27 -0700

    Moved docstring in __init__.py, bump version

--- forallpeople/__init__.py
+++ forallpeople/__init__.py
@@ -1,17 +1,3 @@
-#    Copyright 2020 Connor Ferster
-
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-
-#        http://www.apache.org/licenses/LICENSE-2.0
-
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
-from __future__ import annotations
 """
 The SI Units: "For all people, for all time"
 
@@ -31,8 +17,23 @@ A module to model the seven SI base units:
 
   ...and other derived and non-SI units for practical calculations.
 """
+#    Copyright 2020 Connor Ferster
 
-__version__ = "2.4.2"
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+from __future__ import annotations
+
+
+__version__ = "2.5.0"
 
 from fractions import Fraction
 from typing import Union, Optional
