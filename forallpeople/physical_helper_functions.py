2f7ce766448e2104b2e59b3f9c9a11e1baa1a37b
Connor Ferster <connorferster@gmail.com>
2024-07-12 12:01:41 -0700

    docs: update environment.ipynb file
    - chores: also removed debug prints

--- forallpeople/physical_helper_functions.py
+++ forallpeople/physical_helper_functions.py
@@ -111,16 +111,13 @@ def _evaluate_dims_and_factor(
     # the defined, derived, and default units
     # Once this is done, this feature _should_ be fully implemented
     default = _get_default_unit(dims=dims_orig, units_env=env_dims)
-    print(f"{default=}")
 
     single_dim = _dims_basis_multiple(dims_orig)
 
     # The default-swap logic
     if default and factor != 1:
-        print("First clause")
         default_attributes = list(default.values())[0]
         if not defined and "Factor" in default_attributes:
-            print("Second clause")
             defined = default
             factor = default_attributes["Factor"]
 
